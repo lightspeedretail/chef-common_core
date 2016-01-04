dabfea5d9ee4d767f8b6192e11714befe87363f6
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-01-04 14:30:13 -0500

    Update Policyfiles

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,11 +1,52 @@
 name "common_core"
-
 default_source :supermarket
 
-run_list(
-  "common_core::default"
+# Attributes
+#
+default[:ntp][:servers] = %w(
+  0.amazon.pool.ntp.org
+  1.amazon.pool.ntp.org
+  2.amazon.pool.ntp.org
+  3.amazon.pool.ntp.org
 )
 
+default[:common][:packages].tap do |config|
+  config["awscli"] = true
+  config["ec2-api-tools"] = true
+  config["git-core"] = true
+  config["python-setuptools"] = true
+  config["python-virtualenv"] = true
+  config["python-pip"]        = true
+  config["python-docutils"]   = true
+  config["ruby"] = true
+  config["ruby1.9.1-dev"] = true
+end
+
+default[:common][:auth][:group][:managed][:kibana] = true
+
+default[:common][:linux].tap do |config|
+  config[:sudoers][:ubuntu].tap do |sudoer|
+    sudoer[:user] = "ubuntu"
+    sudoer[:runas] = "root"
+    sudoer[:nopasswd] = true
+  end
+
+  config[:sudoers][:deploy].tap do |sudoer|
+    sudoer[:group] = "deploy"
+    sudoer[:runas] = "root"
+    sudoer[:nopasswd] = true
+    sudoer[:commands] = ["/usr/bin/chef-client"]
+  end
+
+  config[:sudoers]["90-cloud-init-users"].tao do |sudoer|
+    sudoer[:action] = :remove
+  end
+end
+
+default[:authorization][:sudo][:groups] = %w(devops)
+default[:openssh][:server][:allow_groups] = %w(devops deploy vagrant)
+
+run_list("common_core::default")
 cookbook "common_core",   path: "."
 cookbook "common_linux",  path: "../common_linux"
 cookbook "common_auth",   path: "../common_auth"
