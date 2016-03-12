e45d2bb0a6bc588bca75f4cf98daa3f9faea3fb7
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-03-12 13:18:13 -0500

    Update policyfiles

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,41 +1,39 @@
 name "common_core"
 default_source :supermarket
 
-override[:common][:environments][:active] = %w(test)
-override[:common][:namespaces][:active] = %w(test)
+default[:common_attrs][:environments][:active][:prepend] = %w()
+default[:common_attrs][:namespaces][:active][:prepend] = %w()
 
-default[:common][:auth].tap do |auth|
+default[:common_auth].tap do |auth|
   auth[:users][:data_bag] = "auth_users"
   auth[:groups][:data_bag] = "auth_groups"
   auth[:groups][:config][:devops][:action] = "create"
-end
-
-default[:common].tap do |linux|
-  linux[:sudoers][:ubuntu].tap do |sudoer|
+  
+  auth[:sudoers][:ubuntu].tap do |sudoer|
     sudoer[:user] = "ubuntu"
     sudoer[:runas] = "root"
     sudoer[:nopasswd] = true
   end
 
-  linux[:sudoers][:deploy].tap do |sudoer|
+  auth[:sudoers][:deploy].tap do |sudoer|
     sudoer[:group] = "deploy"
     sudoer[:runas] = "root"
     sudoer[:nopasswd] = true
     sudoer[:commands] = %w(/usr/bin/chef-client)
   end
 
-  linux[:sudoers][:devops].tap do |sudoer|
+  auth[:sudoers][:devops].tap do |sudoer|
     sudoer[:group] = "devops"
     sudoer[:runas] = "root"
   end
 
-  linux[:sudoers][:vagrant].tap do |sudoer|
+  auth[:sudoers][:vagrant].tap do |sudoer|
     sudoer[:group] = "vagrant"
     sudoer[:runas] = "root"
     sudoer[:nopasswd] = true
   end
 
-  linux[:sudoers]["90-cloud-init-users"].tap do |sudoer|
+  auth[:sudoers]["90-cloud-init-users"].tap do |sudoer|
     sudoer[:action] = "remove"
   end
 end
@@ -47,6 +45,6 @@ run_list %w(common_core::default)
 cookbook "common_core",   path: "."
 cookbook "common_linux",  path: "../common_linux"
 cookbook "common_auth",   path: "../common_auth"
-cookbook "common_utils",  path: "../common_utils"
+cookbook "common_attrs",  path: "../common_attrs"
 cookbook "rsyslog_ng",    path: "../rsyslog_ng"
 
