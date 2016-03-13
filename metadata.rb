97e81b3f192591d56bdd5561c22edd1c1323f1cb
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-03-13 15:11:11 -0400

    Update urls in README and metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name 'common_core'
 maintainer 'Jonathan Serafini'
 maintainer_email 'jonathan@serafini.ca'
+issues_url 'https://github.com/JonathanSerafini/chef-common_core/issues'
+source_url 'https://github.com/JonathanSerafini/chef-common_core'
 license 'apachev2'
 description 'Installs/Configures common_core'
 long_description 'Installs/Configures common_core'
